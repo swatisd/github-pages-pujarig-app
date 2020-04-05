55530779e1bd6c591d149b83c11f0e71ee6b2967
unknown <deol.swati@gmail.com>
2020-04-06 00:09:42 +0530

    Updates

---
+++ precache-manifest.593e7db507dc5374a1459427bf5d4c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9afcf9f210b5f8ad6cdbfcd25570d4d",
+    "url": "/github-pages-pujarig-app/index.html"
+  },
+  {
+    "revision": "ca3986bd9570f12140d5",
+    "url": "/github-pages-pujarig-app/static/css/main.4a4f4655.chunk.css"
+  },
+  {
+    "revision": "8c34e58e918a29644619",
+    "url": "/github-pages-pujarig-app/static/js/2.417687ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-pujarig-app/static/js/2.417687ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3986bd9570f12140d5",
+    "url": "/github-pages-pujarig-app/static/js/main.7aa8ff5e.chunk.js"
+  },
+  {
+    "revision": "96dcb310b3e9077916a8",
+    "url": "/github-pages-pujarig-app/static/js/runtime-main.e4495e25.js"
+  }
+]);
