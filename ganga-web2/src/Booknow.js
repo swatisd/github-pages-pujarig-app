e16984f60ca0c202905b9726a7497c2f4a10f988
Ruchi Singh <singh.ruchim@gmail.com>
2020-04-12 17:16:06 +0200

    added form submit logic

--- ganga-web2/src/Booknow.js
+++ ganga-web2/src/Booknow.js
@@ -16,12 +16,49 @@ class Booknow extends Component {
       postcode: "",
       customerNeedsNComments: "",
       validationErrors: {},
-      okToSend: false
+      okToSend: false,
+      submitButtonText: "",
+      idToken: "userNotLoggedin"
     };
 
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    // console.log("component did mount");
+    let currentURL = window.location.href;
+    let state = Object.assign({}, this.state);
+    // console.log("state before:" + JSON.stringify(state));
+
+    if (currentURL.indexOf('state') > 0) {
+      state = JSON.parse(atob(currentURL.split('state=')[1].split('&')[0]));
+      state.datetime = new Date(state.datetime);
+    }
+    
+    
+    if (currentURL.indexOf('id_token') > 0) {
+      let params = currentURL.split('#')[1];
+      const idToken = params.split('id_token=')[1].split('&')[0];
+      state.idToken = idToken;
+      const req = new XMLHttpRequest();
+      req.open("GET", "https://api.pujarig.com/d1/user");
+      req.setRequestHeader('Authorization', idToken);
+      req.send();
+      req.onreadystatechange = (e) => {
+        if (req.readyState === 4) {
+          // console.log(req.responseTex);
+          const user = JSON.parse(req.responseText);
+        }
+      }
+    }
+    // console.log("state after parse:" + JSON.stringify(state));
+
+    this.setState(state);
+    // if(currentURL.indexOf('state') > 0){
+    //   window.location = currentURL.split('#')[0];
+    // }
   }
 
   handleDateChange(event) {
@@ -31,20 +68,84 @@ class Booknow extends Component {
         id: "datetime"
       }
     });
-    // this.setState({
-    //   datetime: event,
-    //   validationErrorsdatetime: false
-    // });
   }
 
   handleSubmit(event) {
-    console.log(JSON.stringify(event));
-    alert('Your favorite flavor is: ' + this.state.okToSend);
     event.preventDefault();
+    // console.log("state before submit: " + JSON.stringify(this.state));
+    const preq = {
+      datetime: this.state.datetime.getTime(),
+      pujaName: this.state.ceremony,
+      mobileNumber: this.state.mobile,
+      location: {
+        city: this.state.city,
+        country: this.state.country,
+        postcode: this.state.postcode
+      }
+    }
+    if (this.state.customerNeedsNComments){
+      preq.customerNeedsNComments = this.state.customerNeedsNComments;
+    }
+
+    if (this.state.state){
+      preq.location.state = this.state.state;
+    }
+    // console.log("Data to send: " + JSON.stringify(preq));
+
+    const req = new XMLHttpRequest();
+    req.open("POST", "https://api.pujarig.com/d1/puja", true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.setRequestHeader('Authorization', this.state.idToken);
+    req.send(JSON.stringify(preq));
+    req.onreadystatechange = (e) => {
+      if (req.readyState === 4) {
+        // console.log("req.status: " + req.status);
+        if (req.status === 200) {
+           if(window.location.href.indexOf('state') > 0){
+             window.location = window.location.href.split('&state')[0];
+           }
+          this.setState({
+            vceremony: "",
+            datetime: null,
+            mobile: "",
+            city: "",
+            state: "",
+            country: "",
+            postcode: "",
+            customerNeedsNComments: "",
+            validationErrors: {},
+            submitButtonText:  "Your request is successfully submitted. We will get back to you with the next " +
+            "steps shortly. If you have any queries, please write to admin@pujarig.com",
+            okToSend: false
+          });
+          return false;
+        } else if (req.status === 401 || req.status === 0) {
+          // console.log("got 401 or 0");
+          // console.log(this.loginURI());
+          // console.log("state beofre login: " + JSON.stringify( Object.assign({}, this.state)));
+          window.location = this.loginURI() + "&state=" +
+            btoa(JSON.stringify( Object.assign({}, this.state)));
+         
+        } else if (req.status === 400) {
+          // console.log("req.status === 400");
+          this.setState({
+            submitButtonText:  "Server received invalid input, if the proble persist, please write to admin@pujarig.com",
+            okToSend: false
+          });
+        } else {
+          // console.log("req.status === ***");
+          this.setState({
+            submitButtonText:  "Unknown response from the server, please write to admin@pujarig.com",
+            okToSend: false
+          });
+        }
+      }
+    }
+    
   }
 
   handleInputChange(event) {
-
+    // console.log("state handle input: " + JSON.stringify(this.state));
     const target = event.target;
     const value = target.value;
     const id = target.id;
@@ -93,16 +194,17 @@ class Booknow extends Component {
 
     const ajv = new Ajv({ allErrors: true });
     let validationErrors = Object.assign({}, this.state.validationErrors);
-    console.log(value);
+    // console.log(value);
     let valid = ajv.validate(schema.properties[id], value);
     validationErrors[id] = !valid;
-    console.log(JSON.stringify(validationErrors));
+    // console.log(JSON.stringify(validationErrors));
     const valErrorVals = Object.values(validationErrors);
-    
+
     this.setState({
       [id]: value,
       validationErrors,
-      okToSend: !(valErrorVals.length < schema.required.length ||  valErrorVals.some(v => v === true))
+      okToSend: !(valErrorVals.length < schema.required.length || valErrorVals.some(v => v === true)),
+      submitButtonText: "Check the data before submitting!"
     });
 
   }
@@ -123,13 +225,13 @@ class Booknow extends Component {
                 dateFormat="MMMM d, yyyy h:mm aa"
                 minDate={this.addDays(new Date(), 7)}
               />
-              <small color="red" hidden={!this.state.validationErrors.datetime} id="datetimeHelp" class="form-text text-muted">
+              <small hidden={!this.state.validationErrors.datetime} id="datetimeHelp" class="form-text text-muted">
                 Select date and time.
               </small>
             </div>
             <div class="form-group">
               <label for="exampleFormControlSelect1">Ceremony<sup>*</sup></label>
-              <select class="form-control preq-pname" id="ceremony" onChange={this.handleInputChange}>
+              <select class="form-control preq-pname" id="ceremony" onChange={this.handleInputChange} value={this.state.ceremony}>
                 <option value="">Choose one</option>
                 <option value="satyanarayan">Satyanarayan Vrat Katha</option>
                 <option value="grihPravesh">Grih Pravesh</option>
@@ -146,7 +248,7 @@ class Booknow extends Component {
             <div class="form-group">
               <label for="mobile" >Mobile Number<sup>*</sup></label>
               <input type="tel" class="form-control" id="mobile" aria-describedby="numberhelp" placeholder="Example: +49 151 234567"
-                onChange={(event) => this.handleInputChange(event)}
+                onChange={(event) => this.handleInputChange(event)} value={this.state.mobile}
               />
               <small hidden={!this.state.validationErrors.mobile} id="mobileHelp" class="form-text text-muted">
                 Enter a valid mobile number.
@@ -155,18 +257,18 @@ class Booknow extends Component {
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="city">City<sup>*</sup></label>
-                <input type="text" class="form-control" id="city" onChange={this.handleInputChange} placeholder="Enter City" />
+                <input type="text" class="form-control" id="city" onChange={this.handleInputChange} placeholder="Enter City" value={this.state.city}/>
                 <small hidden={!this.state.validationErrors.city} id="cityHelp" class="form-text text-muted">
                   Enter a valid city name.
               </small>
               </div>
               <div class="form-group col-md-6">
                 <label for="state">State</label>
-                <input type="text" class="form-control" onChange={this.handleInputChange} id="state" placeholder="Enter State" />
+                <input type="text" class="form-control" onChange={this.handleInputChange} id="state" placeholder="Enter State" value={this.state.state}/>
               </div>
               <div class="form-group col-md-6">
                 <label for="country">Country<sup>*</sup></label>
-                <input type="text" class="form-control" id="country" onChange={this.handleInputChange} placeholder="Enter Country" />
+                <input type="text" class="form-control" id="country" onChange={this.handleInputChange} placeholder="Enter Country" value={this.state.country}/>
                 <small hidden={!this.state.validationErrors.country} id="countryHelp" class="form-text text-muted">
                   Enter a valid country name.
               </small>
@@ -174,7 +276,7 @@ class Booknow extends Component {
 
               <div class="form-group col-md-6">
                 <label for="postcode">Post Code<sup>*</sup></label>
-                <input type="text" class="form-control" id="postcode" onChange={this.handleInputChange} placeholder="Enter Post Code" />
+                <input type="text" class="form-control" id="postcode" onChange={this.handleInputChange} placeholder="Enter Post Code" value={this.state.postcode}/>
                 <small hidden={!this.state.validationErrors.postcode} id="postcodeHelp" class="form-text text-muted">
                   Enter a valid postcode.
               </small>
@@ -183,13 +285,14 @@ class Booknow extends Component {
             </div>
             <div class="form-group">
               <label for="customerNeedsNComments">Special Needs / Questions about Puja</label>
-              <textarea class="form-control" rows="3" name="customerNeedsNComments" id="customerNeedsNComments" cols="50" onChange={this.handleInputChange} placeholder="eg: I need puja to be performed according to the north Indian traditions. I would like to know puja the samagri and duration."></textarea>
+              <textarea class="form-control" rows="3" name="customerNeedsNComments" id="customerNeedsNComments" cols="50" onChange={this.handleInputChange} placeholder="eg: I need puja to be performed according to the north Indian traditions. I would like to know puja the samagri and duration." value={this.state.customerNeedsNComments}></textarea>
             </div>
             <div class="form-group">
               <label for="exampleFormControlTextarea1">By clicking 'Submit' button, you agree to the <a class="booktc" href="termsconditions"> Terms and Conditions</a></label>
               <button disabled={!this.state.okToSend} id="submit" class="btn btn-primary btn-lg text-uppercase btnsubmit btn-block" type="submit">Submit</button>
+              {/* <button onSubmit={this.onSubmit} id="submit" class="btn btn-primary btn-lg text-uppercase btnsubmit btn-block" type="submit">Submit</button> */}
               <small hidden={this.state.okToSend} id="submitHelp" class="form-text text-muted">
-                Check the data before submitting!
+                {this.state.submitButtonText}
               </small>
             </div>
           </form>
@@ -205,6 +308,16 @@ class Booknow extends Component {
     return daten;
   }
 
+  loginURI() {
+    return "https://auth.pujarig.com/login?response_type=token&client_id=4f2mhs8n77ceod461gt5cvvhbt&redirect_uri=" +  this.getCallbackUrl();
+  }
+  isProd() {
+    return window.location.href.indexOf('localhost') < 0;
+  }
+  
+  getCallbackUrl() {
+    return (this.isProd() ? 'https://pujarig.com/booknow' : 'http://localhost:3000/booknow');
+  }
 }
 
 export default Booknow;
